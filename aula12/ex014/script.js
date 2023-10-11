4c5c407d9170e52c3f9ce6fcd4988a2d802cfc56
Marcelo Bittencourt <142984146+marcelobitt1982@users.noreply.github.com>
2023-10-11 17:05:06 +0200

    hora do dia

---
+++ aula12/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+var msg = document.getElementById('msg')
+var img = document.getElementById('imagem')
+var data = new Date() 
+//var hora = data.getHours()
+var hora = 20
+msg.innerHTML = `Agora são ${hora} horas.`
+if (hora >= 0 && hora < 12) {
+    //BOM DIA
+    img.src = 'manha.jpg'
+    document.body.style.background = '#fee4bd'
+} else if (hora >= 12 && hora <= 18) {
+    //BOA TARDE
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#ffe263'
+} else {
+    //BOA NOITE
+    img.src = 'noite.jpg'
+    document.body.style.background = '#014f90'
+}
+}
+
