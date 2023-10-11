d650069c279ac9134390ef6cb89c0faf7f730cd3
Marcelo Bittencourt <142984146+marcelobitt1982@users.noreply.github.com>
2023-10-11 18:28:08 +0200

    ex015

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -2,8 +2,8 @@ function carregar() {
 var msg = document.getElementById('msg')
 var img = document.getElementById('imagem')
 var data = new Date() 
-//var hora = data.getHours()
-var hora = 20
+var hora = data.getHours()
+//var hora = 20
 msg.innerHTML = `Agora são ${hora} horas.`
 if (hora >= 0 && hora < 12) {
     //BOM DIA
