ce0f157efb01d402fc6d39262f65031053bea8cf
Marcelo Bittencourt <142984146+marcelobitt1982@users.noreply.github.com>
2023-11-12 15:55:18 +0100

    js

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,7 +1,9 @@
 let amigo = {nome: 'josé',
-sexo: 'M'
+sexo: 'M',
 peso: 85.4,
 engordar(p){
-
+   console.log('Engordou')
+   this.peso += p
 }}
-console.log(amigo)
+
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
